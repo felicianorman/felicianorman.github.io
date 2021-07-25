ba629d29a1d73d03e4822b79d43c7c59c467f9b9
felicianorman <87656480+felicianorman@users.noreply.github.com>
2021-07-25 12:53:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Bild 
+
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -6,5 +8,32 @@ myImage.onclick = function() {
         myImage.setAttribute('src','images/loke2.jpeg');
     } else {
      myImage.setAttribute('src','images/loke.jpeg');
+    
     }
+}
+
+// Personligt meddelande
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Välkommen hit, ' + myName;
+    }      
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+    } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Välkommen hit, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
 }
